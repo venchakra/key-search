226d5abd2e2890541f209167e6b922ac4aa5523a
Venkat Chakravarthy <venchakra@hotmail.com>
2016-11-10 09:27:37 -0800

    Latest version

--- src/search.cpp
+++ src/search.cpp
@@ -1,44 +1,6 @@
 #include "search.h"
 
 SearchResult
-SearchHelper(const int * const items,
-             const int key,
-             const SearchType type,
-             int* const index,
-             int startIndex,
-             int endIndex,
-             int step)
-{
-    SearchResult result = NotFound;
-
-    while (startIndex != endIndex+step) {
-        if ((LessThan == type) && (items[startIndex] < key)) {
-            *index = startIndex;
-            result = FoundLess;
-        } else if ((LessThanEquals == type) && (items[startIndex] <= key)) {
-            *index = startIndex;
-            result = (items[startIndex] == key ? FoundExact : FoundLess);
-        } else if ((Equals == type) && (items[startIndex] == key)) {
-            *index = startIndex;
-            result = FoundExact;
-            break;
-        } else if ((GreaterThanEquals == type) && (items[startIndex] >= key)) {
-            *index = startIndex;
-            result = (items[startIndex] == key ? FoundExact : FoundGreater);
-            break;
-        } else if ((GreaterThan == type) && (items[startIndex] > key)) {
-            *index = startIndex;
-            result = FoundGreater;
-            break;
-        }
-
-        startIndex += step;
-    }
-
-    return result;
-}
-
-SearchResult
 Search(const int * const items,
        const int n_items,
        const int ascending,
@@ -46,12 +8,40 @@ Search(const int * const items,
        const SearchType type,
        int* const index)
 {
-    if (ascending) {
-        // Ascending sorted array
-        return SearchHelper(items, key, type, index, 0, (n_items-1), 1);
+    int startIndex = 0;
+    int endIndex = (n_items-1);
+    int step = 1;
+    SearchResult result = NotFound;
+
+    if (!ascending) {
+        // Descending sorted array (switch start and end indexes so that we
+        // always examine the array in ascending order)
+        startIndex = (n_items-1);
+        endIndex = 0;
+        step = -1;
     }
 
-    // Descending sorted array (switch start and end indexes so that we
-    // examine the array in ascending order)
-    return SearchHelper(items, key, type, index, (n_items-1), 0, -1);
+    for (; startIndex != (endIndex+step); startIndex += step) {
+        if ((LessThan == type) && (items[startIndex] < key)) {
+            *index = startIndex;
+            result = FoundLess;
+        } else if ((LessThanEquals == type) && (items[startIndex] <= key)) {
+            *index = startIndex;
+            result = (items[startIndex] == key) ? FoundExact : FoundLess;
+        } else if ((Equals == type) && (items[startIndex] == key)) {
+            *index = startIndex;
+            result = FoundExact;
+            break;
+        } else if ((GreaterThanEquals == type) && (items[startIndex] >= key)) {
+            *index = startIndex;
+            result = (items[startIndex] == key) ? FoundExact : FoundGreater;
+            break;
+        } else if ((GreaterThan == type) && (items[startIndex] > key)) {
+            *index = startIndex;
+            result = FoundGreater;
+            break;
+        }
+    }
+
+    return result;
 }
